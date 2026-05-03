c6a3e159b32b0e7177117f223bac917e815cb32d
tungdd98 <tungdd98@gmail.com>
2026-05-03 18:29:41 +0700

    feat: generate apps/chatbot with Vite and Vitest

--- apps/chatbot/src/app/app.tsx
+++ apps/chatbot/src/app/app.tsx
@@ -1,13 +1,5 @@
-// Uncomment this line to use CSS modules
-// import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="chatbot" />
-    </div>
-  );
+  return <div>chatbot</div>;
 }
 
 export default App;
