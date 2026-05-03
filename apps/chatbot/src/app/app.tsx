e2c84ab4061f06da50f73bc68c4e347761bd0af5
tungdd98 <tungdd98@gmail.com>
2026-05-03 18:38:51 +0700

    feat: install MUI v5 and configure theme

--- apps/chatbot/src/app/app.tsx
+++ apps/chatbot/src/app/app.tsx
@@ -1,5 +1,15 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+
+import { theme } from '../theme/theme';
+
 export function App() {
-  return <div>chatbot</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>chatbot</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
